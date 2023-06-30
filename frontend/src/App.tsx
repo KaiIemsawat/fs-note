b05022fffe261d59046a35c632abbe02e8daf713
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-06-29 21:12:45 -0400

    setup css base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Note as NoteModel } from "./models/noteModel";
+import Note from "./components/Note";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [notes, setNote] = useState<NoteModel[]>([]);
+
+    useEffect(() => {
+        async function loadNote() {
+            try {
+                const response = await fetch("/api/notes", { method: "GET" });
+                const notes = await response.json();
+                setNote(notes);
+            } catch (error) {
+                console.error(error);
+                alert(error);
+            }
+        }
+        loadNote();
+    }, []);
+
+    return (
+        <Container>
+            <Row xs={1} md={2} xl={3} className="g-4">
+                {notes.map((note) => (
+                    <Col key={note._id}>
+                        <Note note={note} className={styles.note} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
